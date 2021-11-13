c878697a9b15e3f38ea7d7c2fd200e8bd049cbfc
Veronika <v.torubarova.v@gmail.com>
2021-11-13 13:51:47 +0200

    something

---
+++ sprites.py
@@ -0,0 +1,8 @@
+import wrap
+import wrap as UwU, time
+from wrap import world, sprite, actions
+
+world.create_world(1000, 1000)
+sprite.add("mario-enemies",300,200,"dragon_stand1")
+sprite.add("battle_city_tanks",100,300,"tank_enemy_size3_green2")
+sprite.add("mario-scenery",400,500,"tree_large")
